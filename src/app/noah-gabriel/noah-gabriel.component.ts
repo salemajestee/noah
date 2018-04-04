5463a10a890935b76ca7d5df0cb12ada8329372e
SaleMajestee <o8erthe@gmail.com>
2018-04-04 08:56:30 -0400

    florence

--- src/app/noah-gabriel/noah-gabriel.component.ts
+++ src/app/noah-gabriel/noah-gabriel.component.ts
@@ -44,8 +44,29 @@ export class NoahGabrielComponent {
   myInterval = 0;
 
   slides = [
-    {image: '../../assets/images/merci1.jpg'},
-    {image: '../../assets/images/ruby.jpg'}  
+    { image: '../../assets/images/Florence/1.jpg'},
+    { image: '../../assets/images/Florence/2.jpg' },
+    { image: '../../assets/images/Florence/3.jpg' },
+    { image: '../../assets/images/Florence/4.jpg' },
+    { image: '../../assets/images/Florence/5.jpg' },
+    { image: '../../assets/images/Florence/6.jpg' },
+    { image: '../../assets/images/Florence/7.jpg' },
+    { image: '../../assets/images/Florence/8.jpg' },
+    { image: '../../assets/images/Florence/9.jpg' },
+    { image: '../../assets/images/Florence/10.jpg' },
+    { image: '../../assets/images/Florence/11.jpg' },
+    { image: '../../assets/images/Florence/12.jpg' },
+    { image: '../../assets/images/Florence/13.jpg' },
+    { image: '../../assets/images/Florence/14.jpg' },
+    { image: '../../assets/images/Florence/15.jpg' },
+    { image: '../../assets/images/Florence/16.jpg' },
+    { image: '../../assets/images/Florence/17.jpg' },
+    { image: '../../assets/images/Florence/18.jpg' },
+    { image: '../../assets/images/Florence/19.jpg' },
+    { image: '../../assets/images/Florence/20.jpg' },
+    { image: '../../assets/images/Florence/21.jpg' },
+    { image: '../../assets/images/Florence/22.jpg' },
+    { image: '../../assets/images/Florence/23.jpg' },
   ];
 
   
